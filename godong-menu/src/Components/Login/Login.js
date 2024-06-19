2128ce3aa3b91bd1b1c60c27c580cfc3793dabaa
AndhikaW <andhikaw61@gmail.com>
2024-06-19 13:45:18 +0700

    commit

---
+++ godong-menu/src/Components/Login/Login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import "../Login/Login.css"
+
+export default function Login() {
+  return (
+    <div>
+    <head>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
+    </head>
+    <body>
+        <div class="container-fluid login-container">
+            <div class="login-form">
+                <div class="login-box">
+                    <h1 class="text-center">Welcome to <strong>GodongMenu</strong></h1>
+                    <form>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email" placeholder="Email address" />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Password" />
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe" />
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                            <a href="#">Forgot Password?</a>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        <div class="text-center mt-3">
+                            <p>Don't have an account? <a href="#">Sign Up</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="login-image" ></div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</div>
+  )
+}
---
+++ godong-menu/src/Components/SignUp/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../SignUp/SignUp.css"
+export default function SignUp() {
+    return (
+        <div>
+            <head>
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+                />
+            </head>
+        </div>
+    )
+}
